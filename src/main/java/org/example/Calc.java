9fef7803d73ee838b99a70e01991c66a165d1fc4
taeun-kim <outchamp77@gmail.com>
2025-03-19 09:17:45 +0900

    커밋메세지

---
+++ src/main/java/org/example/Calc.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class Calc {
+    public static int run(String exp) {
+
+        exp = exp.replace("- ", "+ -");
+
+        String[] bits = exp.split(" \\+ ");
+
+        int a = Integer.parseInt(bits[0]);
+        int b = Integer.parseInt(bits[1]);
+        int c = 0;
+        int d = 0;
+
+
+        if (bits.length > 2) {
+            c = Integer.parseInt(bits[2]);
+        }
+            return a + b + c;
+        }
+    }
+
+
+
---
+++ src/main/java/org/example/Calctest.java
@@ -0,0 +1,81 @@
+package org.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class Calctest {
+
+    @Test
+    @DisplayName("1 + 1 == 2")
+    public void test1() {
+        assertThat(Calc.run("1 + 1")).isEqualTo(2);
+    }
+    @Test
+    @DisplayName("2 + 1 == 3")
+    public void test2() {
+        assertThat(Calc.run("2 + 1")).isEqualTo(3);
+    }
+    @Test
+    @DisplayName("2 + 2 == 4")
+    public void test3() {
+        assertThat(Calc.run("2 + 2")).isEqualTo(4);
+    }
+    @Test
+    @DisplayName("1000 + 200 == 1200")
+    public void test4() {
+        assertThat(Calc.run("1000 + 200")).isEqualTo(1200);
+    }
+
+    @Test
+    @DisplayName("2 - 1 == 1")
+    public void test5() {
+        assertThat(Calc.run("2 - 1")).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("50 - 30 == 20")
+    public void test6() {
+        assertThat(Calc.run("50 - 30")).isEqualTo(20);
+    }
+
+    @Test
+    @DisplayName("7 - 4 == 3")
+    public void test7() {
+        assertThat(Calc.run("7 - 4")).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("10 + 20 + 30 == 60")
+    public void test8() {
+        assertThat(Calc.run("10 + 20 + 30")).isEqualTo(60);
+    }
+
+    @Test
+    @DisplayName("10 - 20 + 30 == 20")
+    public void test9() {
+        assertThat(Calc.run("10 - 20 + 30")).isEqualTo(20);
+    }
+    @Test
+    @DisplayName("10 - 10 - 10 - 10 + 10 + 10 - 10 == -10")
+    public void test10() {
+        assertThat(Calc.run("10 - 10 - 10 - 10 + 10 + 10 - 10")).isEqualTo(-10);
+    }
+//    @Test
+//    @DisplayName("10 * 10 * 10 == 1000")
+//    public void test11() {
+//        assertThat(Calc.run("10 * 10 * 10")).isEqualTo(1000);
+//    }
+//    @Test
+//    @DisplayName("10 + 5 * 2 == 20")
+//    public void test12() {
+//        assertThat(Calc.run("10 + 5 * 2")).isEqualTo(20);
+//    }
+
+}
+
+
+
+
